714a32d830034cd457a6953aa8a5ad50d330a91a
thanujakaviti <thanujakaviti@gmail.com>
2023-09-27 17:52:14 +0530

    project uploaded

---
+++ src/main/java/in/jt/SbProject2demoApplication.java
@@ -0,0 +1,13 @@
+package in.jt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbProject2demoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbProject2demoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/in/jt/Student.java
@@ -0,0 +1,10 @@
+package in.jt;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+	public Student() {
+		System.out.println("student :: constructo");
+	}
+}
